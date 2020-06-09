8d9363ab23c6e84dc64015c7b8c1c4ac925ca554
unknown <iannsha05@gmail.com>
2020-06-09 22:36:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+import Todoinput from './components/Todoinput'
+import TodoList from './components/TodoList'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {v4 as uuid} from 'uuid'
+
+export default class App extends Component {
+  state={
+    items:[],
+    id:uuid(),
+    title:"",
+    editItem:false
+  }
+
+  handleChange = e =>{
+    this.setState({
+      title:e.target.value
+    })
+  }
+
+  handleSubmit = e =>{
+    e.preventDefault()
+
+    const newItem = {
+      id:this.state.id,
+      title:this.state.title
+    }
+    // console.log(newItem);
+    
+    const updatedItem = [...this.state.items,newItem]
+
+    this.setState({
+      items:updatedItem,
+      title:"",
+      id:uuid(),
+      editItem:false
+    })
+  }
+
+  clearList = () =>{
+    this.setState({
+      items:[]
+    })
+  }
+
+  handleDelete = (id) =>{
+    const filteredItem = this.state.items.filter(item => item.id !== id)
+    this.setState({ items:filteredItem })
+  }
+
+  handleEdit = id =>{
+    const filteredItem = this.state.items.filter(item => item.id !== id)
+    const selectedItem = this.state.items.find(item => item.id === id)
+
+    this.setState({ 
+      items:filteredItem,
+      title:selectedItem.title,
+      editItem:true,
+      id:id
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h3 className="text-capitalize text-center">todo input</h3>
+            <Todoinput title={this.state.title} handleChange={this.handleChange} handleSubmit={this.handleSubmit}
+            editItem={this.state.editItem}/>
+            <TodoList items={this.state.items} clearList={this.clearList} handleDelete={this.handleDelete}
+            handleEdit={this.handleEdit}/>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
